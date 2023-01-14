24eaa253f2ace10732c74055a9c934967476452a
sanjaysan7 <sanjaykv7@gmail.com>
2023-01-14 19:11:03 +0530

    Update script.js
    added js code by manju

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-alert("script working");
+const arr = []
+$("p").each(function(i) {
+  arr.push($(this).text())
+})
+function sort(a, b){
+    const date1 = new Date(a);
+    const date2 = new Date(b);
+    return date1 - date2;
+}
+arr.sort(sort);
+console.log(arr)
